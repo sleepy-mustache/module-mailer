f15b176cb6aaeede6b501196668e03c59e4a97be
Jaime Rodriguez <hi.i.am.jaime@gmail.com>
2015-05-14 12:49:21 -0700

    Fixed a path error for the mailer test

--- mailer_test.php
+++ mailer_test.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__) . '/../../../include/class.debug.php');
+require_once(dirname(__FILE__) . '/../../core/class.debug.php');
 require_once(dirname(__FILE__) . '/class.mailer.php');
 
 /**
