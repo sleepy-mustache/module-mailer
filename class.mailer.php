f1fea5e263f47fca891f868b33efb81a2dcae8b1
Joey <jbomber@envivent.com>
2017-06-19 11:08:08 -0700

    switched over email encoding to UTF-8, allowing for non-ascii characters within emails

--- class.mailer.php
+++ class.mailer.php
@@ -83,7 +83,7 @@ class Message {
 		// To send HTML mail, the Content-type header must be set
 		if ($this->html) {
 			$headers  .= 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 		}
 
 		// Add From
